1e1f813da37fdfb558cbda3ed269908916431efd
hyanhyan <zar.harutyunyan.97@mail.ru>
2019-12-26 08:54:29 +0300

    myproject

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
             var tbody = document.createElement('tbody');
             tbody.setAttribute("id", "tbodyold");
             $(tbody).appendTo(table);
-
+            var tr = document.createElement('tr');
             for (i in theadArr) {
-                var tr = document.createElement('tr');
+                // var tr = document.createElement('tr');
                 var th = document.createElement('th');
                 var a = document.createElement('a');
                 $(a).text(theadArr[i]);
@@ -27,21 +27,28 @@ $(document).ready(function () {
                 tr1.setAttribute('id', 'info');
                 $(tr1).appendTo(tbody);
                 var td1 = document.createElement('td');
+                td1.setAttribute('class', 'name');
                 $(td1).text(data.users[i].name);
                 $(td1).appendTo(tr1);
                 var td2 = document.createElement('td');
+                td2.setAttribute('class', 'surname');
                 $(td2).text(data.users[i].surname);
                 $(td2).appendTo(tr1);
                 var td3 = document.createElement('td');
+                td3.setAttribute('class', 'country');
                 $(td3).text(data.users[i].country);
                 $(td3).appendTo(tr1);
                 var td4 = document.createElement('td');
+                td4.setAttribute('class', 'city');
                 $(td4).text(data.users[i].city);
                 $(td4).appendTo(tr1);
                 var td5 = document.createElement('td');
+                td5.setAttribute('class', 'dateOfBirth');
                 $(td5).text(data.users[i].dateOfBirth);
                 $(td5).appendTo(tr1);
                 var td6 = document.createElement('td');
+                td6.setAttribute('class', 'gender');
+
                 $(td6).text(data.users[i].gender);
                 $(td6).appendTo(tr1);
                 var td7 = document.createElement('td');
@@ -61,6 +68,7 @@ $(document).ready(function () {
                 del.src = data.users[i].image;
                 $(del).appendTo(td8);
                 $(td8).appendTo(tr1);
+                // tr1.setAttribute('data-id', data.users[i]["id"]);
             }
             $(table).appendTo(usersTable);
         }
@@ -77,8 +85,8 @@ var txt = [
         gender: "male",
         img: "img/update.jpg",
         image: "img/delete.jpg",
-        click: "myFunc(1)",
-        onclick: "myFunc(2)"
+        click: "myFunc(1, this)",
+        onclick: "myFunc(2, this)"
 
     }
 ];
@@ -90,80 +98,97 @@ function pushObj() {
         "country": document.getElementById('country').value,
         "city": document.getElementById('city').value,
         "dateOfBirth": document.getElementById('date').value,
-        "gender": $('#gen:checked').value,
+        "gender": $('input:checked',('#gen')).val(),
         "img": "img/update.jpg",
         "image": "img/delete.jpg",
-        "click": "myFunc(1)",
-        "onclick": "myFunc(2)"
+        "click": "myFunc(1, this)",
+        "onclick": "myFunc(2, this)"
     };
-        var tb = document.getElementById("tbodyold");
-        var tbody = document.createElement('tbody');
-        tb.appendChild(tbody);
-        var tr1 = document.createElement('tr');
-        tbody.appendChild(tr1);
-        var td1 = document.createElement('td');
-        $(td1).text(myNewObjElement.name);
-        $(td1).appendTo(tr1);
 
-        var td2 = document.createElement('td');
-        $(td2).text(myNewObjElement.surname);
-        $(td2).appendTo(tr1);
+    var tb = document.getElementById("tbodyold");
+    //var tbody = document.createElement('tbody');
+    //tb.appendChild(tbody);
+    var tr1 = document.createElement('tr');
+    tb.appendChild(tr1);
+    var td1 = document.createElement('td');
+    td1.setAttribute('class', 'name');
+    $(td1).text(myNewObjElement.name);
+    $(td1).appendTo(tr1);
 
-        var td3 = document.createElement('td');
-        $(td3).text(myNewObjElement.country);
-        $(td3).appendTo(tr1);
+    var td2 = document.createElement('td');
+    td2.setAttribute('class', 'surname');
+    $(td2).text(myNewObjElement.surname);
+    $(td2).appendTo(tr1);
 
-        var td4 = document.createElement('td');
-        $(td4).text(myNewObjElement.city);
-        $(td4).appendTo(tr1);
+    var td3 = document.createElement('td');
+    td3.setAttribute('class', 'country');
+    $(td3).text(myNewObjElement.country);
+    $(td3).appendTo(tr1);
 
-        var td5 = document.createElement('td');
-        $(td5).text(myNewObjElement.dateOfBirth);
-        $(td5).appendTo(tr1);
+    var td4 = document.createElement('td');
+    td4.setAttribute('class', 'city');
 
-        var td6 = document.createElement('td');
-        $(td6).text(myNewObjElement.gender);
-        $(td6).appendTo(tr1);
+    $(td4).text(myNewObjElement.city);
+    $(td4).appendTo(tr1);
 
-        var td7 = document.createElement('td');
-        var update = document.createElement('img');
-        update.setAttribute('class', 'homeImg');
-        update.setAttribute('click', myNewObjElement.click);
-        update.src = myNewObjElement.img;
-        $(update).appendTo(td7);
-        $(td7).appendTo(tr1);
+    var td5 = document.createElement('td');
+    td5.setAttribute('class', 'dateOfBirth');
 
-        var td8 = document.createElement('td');
-        var del = document.createElement('img');
-        del.setAttribute('class', 'homeImg');
-        del.src = myNewObjElement.image;
-        del.setAttribute('click', myNewObjElement.onclick);
-        $(del).appendTo(td8);
-        $(td8).appendTo(tr1);
+    $(td5).text(myNewObjElement.dateOfBirth);
 
-        txt.push(myNewObjElement);
-        console.log(txt);
-        document.getElementById('name').value = "";
-        document.getElementById('surname').value = "";
-        document.getElementById('country').value = "";
-        document.getElementById('city').value = "";
-        document.getElementById('date').value = "";
+    $(td5).appendTo(tr1);
 
+    var td6 = document.createElement('td');
+    $(td6).text(myNewObjElement.gender);
+    td6.setAttribute('class', 'gender');
+    $(td6).appendTo(tr1);
 
+    var td7 = document.createElement('td');
+    var update = document.createElement('img');
+    update.setAttribute('class', 'homeImg');
+    update.setAttribute('onclick', myNewObjElement.click);
+    update.src = myNewObjElement.img;
+    $(update).appendTo(td7);
+    $(td7).appendTo(tr1);
+
+    var td8 = document.createElement('td');
+    var del = document.createElement('img');
+    del.setAttribute('class', 'homeImg');
+    del.src = myNewObjElement.image;
+    del.setAttribute('onclick', myNewObjElement.onclick);
+    $(del).appendTo(td8);
+    $(td8).appendTo(tr1);
+
+    txt.push(myNewObjElement);
+    console.log(txt);
+    document.getElementById('name').value = "";
+    document.getElementById('surname').value = "";
+    document.getElementById('country').value = "";
+    document.getElementById('city').value = "";
+    document.getElementById('date').value = "";
 }
 
 
-    function myFunc(x) {
-        if (x === 1) {
-            console.log("trtr");
-        } else if (x === 2) {
-            var result = confirm("Do you want to delete this row?");
-            if (result === true) {
-                document.getElementById('info').remove();
-            } else {
-                alert("Cancelled")
-            }
+function myFunc(x, self) {
+    if (x === 1) {
+        self.parentElement.parentElement.remove();
+        document.getElementById("nav-home-tab").click();
+        console.log(self.parentElement.parentElement.getElementsByClassName("dateOfBirth")[0].innerText);
+        document.getElementById('name').value = self.parentElement.parentElement.getElementsByClassName("name")[0].innerText;
+        document.getElementById('surname').value = self.parentElement.parentElement.getElementsByClassName("surname")[0].innerText;
+        document.getElementById('country').value = self.parentElement.parentElement.getElementsByClassName("country")[0].innerText;
+        document.getElementById('city').value = self.parentElement.parentElement.getElementsByClassName("city")[0].innerText;
+        document.getElementById('date').value = self.parentElement.parentElement.getElementsByClassName("dateOfBirth")[0].innerText;
+
+
+    } else if (x === 2) {
+        var result = confirm("Do you want to delete this row?");
+        if (result === true) {
+            self.parentElement.parentElement.remove();
+        } else {
+            alert("Cancelled")
         }
     }
+}
 
 
