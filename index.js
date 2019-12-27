aa9cbcf5d7e27f3c60e518484cc95ef23a6d3ebf
Zara <zar.harutyunyan.97@mail.ru>
2019-12-27 16:52:36 +0400

    changee

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ var users = [
 function createTable(data) {
     var tableBody = $('#users_table tbody');
     tableBody.html('');
+
     if(data) {
         data.forEach((el, i)=>{
             var tr = `<tr>
@@ -41,7 +42,7 @@ function createTable(data) {
 			<td>${el.city}</td>
 			<td>${el.dateOfBirth}</td>
 			<td>${el.gender}</td>
-			<td><img src="img/delete.jpg" class="homeImg" onclick='delFunc(${i})'><img src="img/update.jpg" class="homeImg" onclick='editUser(${i})'></td>
+			<td><img src="img/delete.jpg" class="homeImg" id="info" onclick='delFunc(${i})'><img src="img/update.jpg" class="homeImg" onclick='editUser(${i})'></td>
 			</tr>`;
             tableBody.append(tr)
         })
@@ -61,15 +62,46 @@ function pushObj() {
     users.push(myNewObjElement);
     createTable(users);
     document.getElementById('nav-profile-tab').click();
+    document.getElementById('name').value="";
+    document.getElementById('surname').value="";
+    document.getElementById('country').value="";
+    document.getElementById('city').value="";
+    document.getElementById('date').value="";
+
 }
+// function myFunc(x, self) {
+//     if (x === 1) {
+//         self.parentElement.parentElement.remove();
+//         document.getElementById("nav-home-tab").click();
+//         console.log(self.parentElement.parentElement.getElementsByClassName("dateOfBirth")[0].innerText);
+//         document.getElementById('name').value = self.parentElement.parentElement.getElementsByClassName("name")[0].innerText;
+//         document.getElementById('surname').value = self.parentElement.parentElement.getElementsByClassName("surname")[0].innerText;
+//         document.getElementById('country').value = self.parentElement.parentElement.getElementsByClassName("country")[0].innerText;
+//         document.getElementById('city').value = self.parentElement.parentElement.getElementsByClassName("city")[0].innerText;
+//         document.getElementById('date').value = self.parentElement.parentElement.getElementsByClassName("dateOfBirth")[0].innerText;
+//
+//
+//     } else if (x === 2) {
+//         var result = confirm("Do you want to delete this row?");
+//         if (result === true) {
+//             self.parentElement.parentElement.remove();
+//         } else {
+//             alert("Cancelled")
+//         }
+//     }
+// }
 
 function delFunc(i) {
     var result = confirm("Do you want to delete this row?");
     if (result === true) {
-        users.splice(i, 1);
-        createTable(users);
+            users.splice(i,1);
+            createTable(users);
+    }
+        else {
+        alert("Cancelled")
     }
 }
+
 function editUser(i) {
     $('#name').val(users[i].name);
     $('#surname').val(users[i].surname);
@@ -82,6 +114,7 @@ function editUser(i) {
     });
     document.getElementById('nav-home-tab').click();
 }
+
 function updateUser(i) {
     users[i] = {};
     users[i].id = users.length;
@@ -92,6 +125,19 @@ function updateUser(i) {
     users[i].dateOfBirth = $('#date').val();
     users[i].gender = $("input[name='gender']:checked").val();
     createTable(users);
+    document.getElementById('nav-profile-tab').click();
 }
 
+
+
+function validate() {
+    var name = document.getElementById('name');
+    if ((name.value == null || name.value === "") && (typeof name.value !=('text'))) {
+        name.style.borderColor = "red";
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
