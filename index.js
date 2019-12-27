5134a60ce1975bdc79d27c0144af56e424492173
hyanhyan <zar.harutyunyan.97@mail.ru>
2019-12-27 11:41:54 +0300

    my new changes

--- index.js
+++ index.js
@@ -1,194 +1,97 @@
-var usersTable = document.getElementById('nav-profile');
-var theadArr = ['name', 'surname', 'country', 'city', 'birthday', 'gender', '', ''];
-$(document).ready(function () {
-    $.ajax({
-        url: "json/users.json",
-        dataType: "json",
-        success: function (data) {
-            var table = document.createElement('table');
-            var thead = document.createElement('thead');
-            $(thead).appendTo(table);
-            var tbody = document.createElement('tbody');
-            tbody.setAttribute("id", "tbodyold");
-            $(tbody).appendTo(table);
-            var tr = document.createElement('tr');
-            for (i in theadArr) {
-                // var tr = document.createElement('tr');
-                var th = document.createElement('th');
-                var a = document.createElement('a');
-                $(a).text(theadArr[i]);
-                $(a).appendTo(th);
-                $(th).appendTo(tr);
-                $(tr).appendTo(thead);
-            }
-
-            for (var i in data.users) {
-                var tr1 = document.createElement('tr');
-                tr1.setAttribute('id', 'info');
-                $(tr1).appendTo(tbody);
-                var td1 = document.createElement('td');
-                td1.setAttribute('class', 'name');
-                $(td1).text(data.users[i].name);
-                $(td1).appendTo(tr1);
-                var td2 = document.createElement('td');
-                td2.setAttribute('class', 'surname');
-                $(td2).text(data.users[i].surname);
-                $(td2).appendTo(tr1);
-                var td3 = document.createElement('td');
-                td3.setAttribute('class', 'country');
-                $(td3).text(data.users[i].country);
-                $(td3).appendTo(tr1);
-                var td4 = document.createElement('td');
-                td4.setAttribute('class', 'city');
-                $(td4).text(data.users[i].city);
-                $(td4).appendTo(tr1);
-                var td5 = document.createElement('td');
-                td5.setAttribute('class', 'dateOfBirth');
-                $(td5).text(data.users[i].dateOfBirth);
-                $(td5).appendTo(tr1);
-                var td6 = document.createElement('td');
-                td6.setAttribute('class', 'gender');
-
-                $(td6).text(data.users[i].gender);
-                $(td6).appendTo(tr1);
-                var td7 = document.createElement('td');
-                td7.setAttribute('id', 'info');
-                var update = document.createElement('img');
-                update.setAttribute('class', 'homeImg');
-                update.setAttribute('onclick', data.users[i].click);
-                update.src = data.users[i].img;
-                $(update).appendTo(td7);
-                $(td7).appendTo(tr1);
-                var td8 = document.createElement('td');
-                td8.setAttribute('id', 'all');
-                var del = document.createElement('img');
-                del.setAttribute('id', 'alll');
-                del.setAttribute('class', 'homeImg');
-                del.setAttribute('onclick', data.users[i].onclick);
-                del.src = data.users[i].image;
-                $(del).appendTo(td8);
-                $(td8).appendTo(tr1);
-                // tr1.setAttribute('data-id', data.users[i]["id"]);
-            }
-            $(table).appendTo(usersTable);
-        }
-    });
-});
-
-var txt = [
+var users = [
     {
-        name: "John",
-        surname: "Smith",
-        country: "USA",
-        city: "New York",
-        dateOfBirth: "01/10/1990",
-        gender: "male",
-        img: "img/update.jpg",
-        image: "img/delete.jpg",
-        click: "myFunc(1, this)",
-        onclick: "myFunc(2, this)"
-
+        "id": 0,
+        "name": "John",
+        "surname": "Smith",
+        "country": "USA",
+        "city": "New York",
+        "dateOfBirth": "1990-10-01",
+        "gender": "male"
+    },
+    {
+        "id": 1,
+        "name": "Poghos",
+        "surname": "Poghosyan",
+        "country": "Armenia",
+        "city": "Gyumri",
+        "dateOfBirth": "1999-11-12",
+        "gender": "male"
+    },
+    {
+        "id": 2,
+        "name": "John",
+        "surname": "Doe",
+        "country": "England",
+        "city": "London",
+        "dateOfBirth": "1995-01-01",
+        "gender": "male"
     }
 ];
 
-function pushObj() {
-    let myNewObjElement = {
-        "name": document.getElementById('name').value,
-        "surname": document.getElementById('surname').value,
-        "country": document.getElementById('country').value,
-        "city": document.getElementById('city').value,
-        "dateOfBirth": document.getElementById('date').value,
-        "gender": $('input:checked',('#gen')).val(),
-        "img": "img/update.jpg",
-        "image": "img/delete.jpg",
-        "click": "myFunc(1, this)",
-        "onclick": "myFunc(2, this)"
-    };
-
-    var tb = document.getElementById("tbodyold");
-    //var tbody = document.createElement('tbody');
-    //tb.appendChild(tbody);
-    var tr1 = document.createElement('tr');
-    tb.appendChild(tr1);
-    var td1 = document.createElement('td');
-    td1.setAttribute('class', 'name');
-    $(td1).text(myNewObjElement.name);
-    $(td1).appendTo(tr1);
-
-    var td2 = document.createElement('td');
-    td2.setAttribute('class', 'surname');
-    $(td2).text(myNewObjElement.surname);
-    $(td2).appendTo(tr1);
-
-    var td3 = document.createElement('td');
-    td3.setAttribute('class', 'country');
-    $(td3).text(myNewObjElement.country);
-    $(td3).appendTo(tr1);
-
-    var td4 = document.createElement('td');
-    td4.setAttribute('class', 'city');
-
-    $(td4).text(myNewObjElement.city);
-    $(td4).appendTo(tr1);
-
-    var td5 = document.createElement('td');
-    td5.setAttribute('class', 'dateOfBirth');
-
-    $(td5).text(myNewObjElement.dateOfBirth);
-
-    $(td5).appendTo(tr1);
-
-    var td6 = document.createElement('td');
-    $(td6).text(myNewObjElement.gender);
-    td6.setAttribute('class', 'gender');
-    $(td6).appendTo(tr1);
-
-    var td7 = document.createElement('td');
-    var update = document.createElement('img');
-    update.setAttribute('class', 'homeImg');
-    update.setAttribute('onclick', myNewObjElement.click);
-    update.src = myNewObjElement.img;
-    $(update).appendTo(td7);
-    $(td7).appendTo(tr1);
-
-    var td8 = document.createElement('td');
-    var del = document.createElement('img');
-    del.setAttribute('class', 'homeImg');
-    del.src = myNewObjElement.image;
-    del.setAttribute('onclick', myNewObjElement.onclick);
-    $(del).appendTo(td8);
-    $(td8).appendTo(tr1);
-
-    txt.push(myNewObjElement);
-    console.log(txt);
-    document.getElementById('name').value = "";
-    document.getElementById('surname').value = "";
-    document.getElementById('country').value = "";
-    document.getElementById('city').value = "";
-    document.getElementById('date').value = "";
+function createTable(data) {
+    var tableBody = $('#users_table tbody');
+    tableBody.html('');
+    if(data) {
+        data.forEach((el, i)=>{
+            var tr = `<tr>
+			<td>${el.id}</td>
+			<td>${el.name}</td>
+			<td>${el.surname}</td>
+			<td>${el.country}</td>
+			<td>${el.city}</td>
+			<td>${el.dateOfBirth}</td>
+			<td>${el.gender}</td>
+			<td><img src="img/delete.jpg" class="homeImg" onclick='delFunc(${i})'><img src="img/update.jpg" class="homeImg" onclick='editUser(${i})'></td>
+			</tr>`;
+            tableBody.append(tr)
+        })
+    }
 }
 
+createTable(users);
 
-function myFunc(x, self) {
-    if (x === 1) {
-        self.parentElement.parentElement.remove();
-        document.getElementById("nav-home-tab").click();
-        console.log(self.parentElement.parentElement.getElementsByClassName("dateOfBirth")[0].innerText);
-        document.getElementById('name').value = self.parentElement.parentElement.getElementsByClassName("name")[0].innerText;
-        document.getElementById('surname').value = self.parentElement.parentElement.getElementsByClassName("surname")[0].innerText;
-        document.getElementById('country').value = self.parentElement.parentElement.getElementsByClassName("country")[0].innerText;
-        document.getElementById('city').value = self.parentElement.parentElement.getElementsByClassName("city")[0].innerText;
-        document.getElementById('date').value = self.parentElement.parentElement.getElementsByClassName("dateOfBirth")[0].innerText;
+function pushObj() {
+    var myNewObjElement = {};
+    myNewObjElement.id=users.length;
+    myNewObjElement.name=$(`#name`).val();
+    myNewObjElement.surname=$(`#surname`).val();
+    myNewObjElement.country=$(`#country`).val();
+    myNewObjElement.city=$(`#city`).val();
+    myNewObjElement.gender = $("input[name='gender']:checked").val();
+    users.push(myNewObjElement);
+    createTable(users);
+    document.getElementById('nav-profile-tab').click();
+}
 
-
-    } else if (x === 2) {
-        var result = confirm("Do you want to delete this row?");
-        if (result === true) {
-            self.parentElement.parentElement.remove();
-        } else {
-            alert("Cancelled")
-        }
+function delFunc(i) {
+    var result = confirm("Do you want to delete this row?");
+    if (result === true) {
+        users.splice(i, 1);
+        createTable(users);
     }
+}
+function editUser(i) {
+    $('#name').val(users[i].name);
+    $('#surname').val(users[i].surname);
+    $('#country').val(users[i].country);
+    $('#city').val(users[i].city);
+    $('#date').val(users[i].dateOfBirth);
+    $(`#${users[i].gender}`).prop("checked", true);
+    $("#submit_button").text('Update').prop( "onclick", null ).click(function() {
+        updateUser(i);
+    });
+    document.getElementById('nav-home-tab').click();
+}
+function updateUser(i) {
+    users[i] = {};
+    users[i].id = users.length;
+    users[i].name = $('#name').val();
+    users[i].surname = $('#surname').val();
+    users[i].country = $('#country').val();
+    users[i].city = $('#city').val();
+    users[i].dateOfBirth = $('#date').val();
+    users[i].gender = $("input[name='gender']:checked").val();
+    createTable(users);
 }
 
 
